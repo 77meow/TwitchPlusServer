3d50659de8d1f0272c71b69c6bd3d216e27cccc2
77meow <wenqil@andrew.cmu.edu>
2021-01-21 16:14:01 -0500

    code upload

---
+++ src/main/java/com/laioffer/jupiter/entity/ItemType.java
@@ -0,0 +1,8 @@
+package com.laioffer.jupiter.entity;
+
+public enum ItemType {
+    STREAM, VIDEO, CLIP;
+}
+
+// ItemType t = ItemType.STREAM;
+// ItemType t = ItemType.Vincent <- error
---
+++ src/main/java/com/laioffer/jupiter/servlet/RecommendationServlet.java
@@ -0,0 +1,39 @@
+package com.laioffer.jupiter.servlet;
+
+import com.laioffer.jupiter.entity.Item;
+import com.laioffer.jupiter.recommendation.ItemRecommender;
+import com.laioffer.jupiter.recommendation.RecommendationException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "RecommendationServlet", urlPatterns = {"/recommendation"})
+public class RecommendationServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false); // 判断用户登录信息
+        ItemRecommender itemRecommender = new ItemRecommender();
+        Map<String, List<Item>> itemMap;
+        try {
+            if (session == null) { // 如果没有登录 就by default
+                itemMap = itemRecommender.recommendItemsByDefault();
+            } else {
+                String userId = (String) request.getSession().getAttribute("user_id");
+                itemMap = itemRecommender.recommendItemsByUser(userId);
+            }
+        } catch (RecommendationException e) {
+            throw new ServletException(e);
+        }
+
+        ServletUtil.writeItemMap(response, itemMap);
+    }
+}
+
+
